47fb4d000be45577334974d15ec312c4551460da
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-10 10:14:47 +0530

    add responsiveness

--- src/components/OneTransaction.jsx
+++ src/components/OneTransaction.jsx
@@ -8,13 +8,13 @@ const OneTransaction = ({ transaction }) => {
                 <div className='flex items-center gap-4'>
                     <img src={transaction.details.photoURL} alt="" className="rounded-[100px] border border-violet-900 md:h-14 h-10" />
                     <div className='flex flex-col '>
-                        <p className='text-lg md:text-xl'>{transaction.details.actionFrom}</p>
-                        <p className='text-base md:text-md'>{transaction.details.name}</p>
+                        <p className='text-base sm:text-lg md:text-xl'>{transaction.details.actionFrom}</p>
+                        <p className='text-xs sm:text-sm md:text-base'>{transaction.details.name}</p>
                     </div>
                 </div>
                 <div className='flex flex-col items-end justify-end'>
-                    {transaction.details.actionFrom === 'Sent to' ? <p className='font-bold text-red-700 text-2xl'>-₹{(transaction.details.amountToPay).toLocaleString()}</p> : <p className='font-bold text-green-900 text-2xl'>+₹{(transaction.details.amountToPay).toLocaleString()}</p>}
-                    <p className='text-sm'>{transaction.date}</p>
+                    {transaction.details.actionFrom === 'Sent to' ? <p className='font-bold text-red-700 sm:text-xl text-lg md:text-2xl'>-₹{(transaction.details.amountToPay).toLocaleString()}</p> : <p className='font-bold text-green-900 sm:text-xl text-lg md:text-2xl'>+₹{(transaction.details.amountToPay).toLocaleString()}</p>}
+                    <p className='text-xs md:text-sm'>{transaction.date}</p>
                 </div>
             </div>
             <hr />
